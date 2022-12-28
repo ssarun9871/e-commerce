8c05121bd40e1145efe28c3992b1ca87ff58db6d
ssarun9871 <ssarun9871@gmail.com>
2022-12-29 01:14:40 +0530

    desigining user and product relationship

--- app.js
+++ app.js
@@ -8,42 +8,59 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
 const contactus = require('./routes/contactus');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //this is important to serve css statically
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req,res,next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>console.log(err));
+})
 
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
-app.set('view engine','ejs');
-app.set('views','views');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-// db.execute('select * from products')
-// .then(result=>{
-//    // console.log(result)
-// })
-// .catch(err=>{
-//     console.log(err);
-// });
-
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(contactus);
 
-app.use((req,res,next)=>{
-    res.status(404).render('404',{
-        pageTitle:'page not found!!',
+app.use((req, res, next) => {
+    res.status(404).render('404', {
+        pageTitle: 'page not found!!',
         path: ''
     })
 })
 
-    sequelize.sync().then(result=>{
-       // console.log(result);
-        app.listen(3000); 
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+Product.belongsTo(User,{constraints:true , onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+.sync()
+.then(result => {
+    return User.findByPk(1);
+})
+.then(user=>{
+    if(!user){
+       return User.create({name:'Max', email:'test@test.com'});
+    }
+    return user;
+})
+.then(user=>{
+   // console.log(user);
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
 
 
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -4,9 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   });
 };
 
@@ -15,15 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
     .then(result => {
-      console.log('Product created');
-      res.redirect('/admin/products')
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
     })
     .catch(err => {
       console.log(err);
@@ -36,8 +36,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findById(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -73,7 +76,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -85,13 +89,16 @@ exports.getProducts = (req, res, next) => {
 };
 
 
+
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findByPk(prodId)
-  .then(product=>{
-    return product.destroy();
-  })
-  .then(result=>{console.log('product deleted')})
-  .catch(err=>console.log(err));
-  res.redirect('/admin/products');
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log('DESTROYED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
