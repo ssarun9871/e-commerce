6e4915d20a39850ccef525165e438d9f9a277a32
ssarun9871 <ssarun9871@gmail.com>
2023-02-06 16:02:01 +0530

    integration of ordering function

--- models/user.js
+++ models/user.js
@@ -48,11 +48,16 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 };
 
-userSchema.methods.removeFromCart = function(){
-  const updatedCartItems = this.cart.items.filter(item=>{
-    return item.productId.toString()!==productId.toString();
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
   });
-  this.cart.itmes = updatedCartItems;
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
   return this.save();
 };
 
